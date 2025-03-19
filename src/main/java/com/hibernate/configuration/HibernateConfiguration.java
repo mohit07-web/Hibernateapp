19372894b73048b34222b2c2b812bdbbc5de6738
mohit07-web <jrrmohit2052@gmail.com>
2025-03-19 15:25:37 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/configuration/HibernateConfiguration.java
@@ -0,0 +1,28 @@
+package com.hibernate.configuration;
+import java.util.Properties;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Environment;
+
+public class HibernateConfiguration {
+
+	public static SessionFactory getSessionFactory() {
+		Properties ps = new Properties();
+		ps.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+		ps.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://Localhost:3306/mydatabase");
+		ps.put(Environment.JAKARTA_JDBC_USER, "root");
+		ps.put(Environment.JAKARTA_JDBC_PASSWORD, "root");
+		ps.put(Environment.HBM2DDL_AUTO, "update");
+		ps.put(Environment.SHOW_SQL, "true");
+		ps.put(Environment.FORMAT_SQL, "true");
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().applySettings(ps).build();
+//		MetadataSources  metadataSources = new MetadataSources(ssr).addAnnotatedClass(com.hibernate.entity.Employee.class);
+//		SessionFactory sessionFactory = metadataSources.getMetadataBuilder().build().buildSessionFactory();
+		return new MetadataSources(new StandardServiceRegistryBuilder().applySettings(ps).build())
+				.addAnnotatedClasses(com.hibernate.entity.Employee.class, com.hibernate.entity.Address.class)
+				.getMetadataBuilder().build().buildSessionFactory();
+	}
+
+}
---
+++ src/main/java/com/hibernate/entity/Employee.java
@@ -0,0 +1,82 @@
+package com.hibernate.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String gender;
+	private int salary;
+
+	@ManyToMany(mappedBy="employee")
+	List<Address> address;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, String gender, int salary, Address address) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.salary = salary;
+		this.address = address;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
+	}
+
+	
+
+}
